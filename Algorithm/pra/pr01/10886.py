e287afe7e318e8b7486c4268564adfde2403567b
iblug <1115jkb@gmail.com>
2023-01-16 18:04:38 +0900

    fix

--- Algorithm/pra/pr01/10886.py
+++ Algorithm/pra/pr01/10886.py
@@ -2,15 +2,36 @@
 # 0 = not cute / 1 = cute
 
 import sys
+import time
 sys.stdin = open('input.txt', 'r')
 
+
 n = int(input())
+start = time.time()
+'''
 s = ''
 for _ in range(n):
-    s += input().split()
+    s += input()
+    # s += sys.stdin.readline().rstrip()
 # print(s)
 # print(s.count('0'))
-if s.count('0') > len(s) // 2:
+if s.count('0') > n // 2:
     print('Junhee is not cute!')
 else:
     print('Junhee is cute!')
+
+'''
+# 리스트를 쓰거나 변수 들어올때마다 cnt하자
+
+cnt = 0
+for _ in range(n):
+    if input() == '0':
+        cnt = 1
+if cnt > n // 2:
+    print('Junhee is not cute!')
+else:
+    print('Junhee is cute!')
+
+
+end = time.time()
+print(end - start)
