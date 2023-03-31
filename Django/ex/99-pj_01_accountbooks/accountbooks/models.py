08d97739c63597e18ff40157ed03f50d984ab713
iblug <1115jkb@gmail.com>
2023-03-31 19:19:45 +0900

    Add Date Filter

--- Django/ex/99-pj_01_accountbooks/accountbooks/models.py
+++ Django/ex/99-pj_01_accountbooks/accountbooks/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class AccountBook(models.Model):
-    note = models.CharField(max_length=80)
-    description = models.TextField(null=True)
-    category = models.CharField(max_length=50)
-    amount = models.IntegerField(default=0)
-    date = models.DateField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    note = models.CharField(max_length=80) # 내용
+    description = models.TextField(null=True) # 메모
+    category = models.CharField(max_length=50) # 분류
+    amount = models.IntegerField(default=0) # 사용 금액
+    date = models.DateField() # 사용 날짜
+    created_at = models.DateTimeField(auto_now_add=True) # 생성 날짜
+    updated_at = models.DateTimeField(auto_now=True) # 수정 날짜
