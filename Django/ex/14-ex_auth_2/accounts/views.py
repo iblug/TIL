2a3ca115b8b47c37c9a893c938c21ab52688e2b7
iblug <1115jkb@gmail.com>
2023-04-06 23:39:51 +0900

    Create Django_14_ex_Auth

---
+++ Django/ex/14-ex_auth_2/accounts/views.py
@@ -0,0 +1,91 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth import update_session_auth_hash
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+def login(request):
+    next_url = request.GET.get('next','')
+    # print(next_url)
+    if request.user.is_authenticated:
+        return redirect('articles:index')
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            next_url = request.POST.get('next', '')
+            # print(next_url)
+            if next_url:
+                return redirect(next_url)
+            else:
+                return redirect('articles:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form': form,
+        'next': next_url,
+    }
+    return render(request, 'accounts/login.html', context)
+
+@login_required
+def logout(request):
+    auth_logout(request)
+    return redirect('articles:index')
+
+
+def signup(request):
+    if request.user.is_authenticated:
+        return redirect('articles:index')
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('articles:index')
+    else:
+        form = CustomUserCreationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/signup.html', context)
+
+@login_required
+def delete(request):
+    # print(dir(request.user))
+    request.user.delete()
+    # 탈퇴하면서 유저의 세션 정보도 함께 지우고 싶을 경우 # 탈퇴 > 로그아웃 순서 중요
+    auth_logout(request)
+    return redirect('articles:index')
+
+@login_required
+def update(request):
+    if request.method == 'POST':
+        form = CustomUserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:index')
+    else:
+        form = CustomUserChangeForm(instance=request.user)
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/update.html', context)
+
+@login_required
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            # 비밀번호 변경시 세션 무효화 방지
+            update_session_auth_hash(request, user)
+            return redirect('articles:index')
+    else:
+        form = PasswordChangeForm(request.user)
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/change_password.html', context)
