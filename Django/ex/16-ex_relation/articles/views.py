32e00ca38734bec4886762b8f55b8c2e7ee80c9b
iblug <1115jkb@gmail.com>
2023-04-11 20:56:00 +0900

    Create Django_16-ex

---
+++ Django/ex/16-ex_relation/articles/views.py
@@ -0,0 +1,91 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Article, Comment
+from .forms import ArticleForm, CommentForm
+
+
+# Create your views here.
+def index(request):
+    articles = Article.objects.all()
+    context = {
+        'articles': articles,
+    }
+    return render(request, 'articles/index.html', context)
+
+
+def detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    comment_form = CommentForm()
+    comments = article.comment_set.all()
+    context = {
+        'article': article,
+        'comment_form': comment_form,
+        'comments': comments,
+    }
+    return render(request, 'articles/detail.html', context)
+
+
+@login_required
+def create(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            article = form.save()
+            return redirect('articles:detail', article.pk)
+    else:
+        form = ArticleForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'articles/create.html', context)
+
+
+@login_required
+def delete(request, artilce_pk):
+    article = Article.objects.get(pk=artilce_pk)
+
+    article.delete()
+
+    return redirect('articles:index')
+
+
+@login_required
+def update(request, article_pk):
+    article = Article.objects.get(pk=article_pk)
+    if request.method == 'POST':
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:detail', article.pk)
+    else:
+        form = ArticleForm(instance=article)
+    context = {
+        'article': article,
+        'form': form,
+    }
+    return render(request, 'articles/update.html', context)
+
+
+def comment_create(request, article_pk):
+    # 몇 번 게시글인지 조회
+    article = Article.objects.get(pk=article_pk)
+    # 댓글 데이터를 받아서
+    comment_form = CommentForm(request.POST)
+    # 유효성 검증
+    if comment_form.is_valid():
+        # commit을 False로 지정하면 인스턴스는 반환하면서도 DB에 레코드는 작성하지 않도록 함
+        comment = comment_form.save(commit=False)
+        comment.article = article
+        comment_form.save() # default : commit=True
+        return redirect('articles:detail', article.pk)
+    context = {
+        'article': article,
+        'comment_form': comment_form,
+    }
+    return render(request, 'articles/detail', context)
+
+
+def comments_delete(request, article_pk, comment_pk):
+    comment = Comment.objects.get(pk=comment_pk)
+    comment.delete()
+    return redirect('articles:detail', article_pk)
