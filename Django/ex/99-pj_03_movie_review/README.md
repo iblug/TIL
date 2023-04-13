3161daf6026932dda21570dce95d5f036923ffcd
iblug <1115jkb@gmail.com>
2023-04-13 17:45:48 +0900

    Create Project_Django-movie_review

---
+++ Django/ex/99-pj_03_movie_review/README.md
@@ -0,0 +1,2 @@
+# Movie_Review
+멀티캠퍼스 github을 활용한 미니 프로젝트
---
+++ Django/ex/99-pj_03_movie_review/reviews/views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import render, redirect
+from .models import Review
+from .forms import ReviewForm
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+def index(request):
+    reviews = Review.objects.all()
+    context = {
+        'reviews': reviews,
+    }
+    return render(request, 'reviews/index.html', context)
+
+
+@login_required
+def create(request):
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.user = request.user
+            review.save()
+            return redirect('reviews:index')
+    else:
+        form = ReviewForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'reviews/create.html', context)
+
+
+def detail(request, review_pk):
+    review = Review.objects.get(pk=review_pk)
+    context = {
+        'review': review,
+    }
+    return render(request, 'reviews/detail.html', context)
+
+
+@login_required
+def update(request, review_pk):
+    review = Review.objects.get(pk=review_pk)
+    
+    if request.user == review.user:
+        if request.method == 'POST':
+            form = ReviewForm(request.POST, instance=review)
+            if form.is_valid():
+                form.save()
+                return redirect('reviews:detail', review.pk)
+        else:
+            form = ReviewForm(instance=review)
+    else:
+        return redirect('reviews:index')
+    context = {
+        'review': review,
+        'form': form,
+    }
+    return render(request, 'reviews/update.html', context)
+
+
+@login_required
+def delete(request, review_pk):
+    review = Review.objects.get(pk=review_pk)
+    if request.user == review.user:
+        review.delete()
+    return redirect('reviews:index')
