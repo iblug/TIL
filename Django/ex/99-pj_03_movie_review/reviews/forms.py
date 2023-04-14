c618989efa462e476ccc51463c87280dcd80af27
iblug <1115jkb@gmail.com>
2023-04-14 18:20:14 +0900

    Update Project_Django-movie_review

--- Django/ex/99-pj_03_movie_review/reviews/forms.py
+++ Django/ex/99-pj_03_movie_review/reviews/forms.py
@@ -1,7 +1,45 @@
 from django import forms
-from .models import Review
+from .models import Review, Comment
 
 class ReviewForm(forms.ModelForm):
+    title = forms.CharField(
+        label='제목',
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': '제목을 입력해주세요',
+            }
+        ),
+    )
+    content = forms.CharField(
+        label='내용',
+        widget=forms.Textarea(
+            attrs={
+                'class': 'form-control',
+                'placeholder': '내용을 입력해주세요',
+            }
+        ),
+    )
+    movie = forms.CharField(
+        label='영화 이름',
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': '영화이름을 입력해주세요',
+            }
+        ),
+    )
+    image = forms.ImageField(
+        label='사진',
+        widget=forms.FileInput(
+            
+        )
+    )
     class Meta:
         model = Review
-        fields = ('title', 'content', 'movie',)
+        fields = ('title', 'content', 'movie', 'image',)
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('content',)
--- Django/ex/99-pj_03_movie_review/reviews/views.py
+++ Django/ex/99-pj_03_movie_review/reviews/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, redirect
-from .models import Review
-from .forms import ReviewForm
+from .models import Review, Comment
+from .forms import ReviewForm, CommentForm
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
-    reviews = Review.objects.all()
+    reviews = Review.objects.all().order_by('-pk')
     context = {
         'reviews': reviews,
     }
@@ -15,7 +15,7 @@ def index(request):
 @login_required
 def create(request):
     if request.method == 'POST':
-        form = ReviewForm(request.POST)
+        form = ReviewForm(request.POST, request.FILES)
         if form.is_valid():
             review = form.save(commit=False)
             review.user = request.user
@@ -31,8 +31,12 @@ def create(request):
 
 def detail(request, review_pk):
     review = Review.objects.get(pk=review_pk)
+    comment_form = CommentForm()
+    comments = review.comment_set.all()
     context = {
         'review': review,
+        'comment_form': comment_form,
+        'comments': comments,
     }
     return render(request, 'reviews/detail.html', context)
 
@@ -43,7 +47,7 @@ def update(request, review_pk):
     
     if request.user == review.user:
         if request.method == 'POST':
-            form = ReviewForm(request.POST, instance=review)
+            form = ReviewForm(request.POST, request.FILES, instance=review)
             if form.is_valid():
                 form.save()
                 return redirect('reviews:detail', review.pk)
@@ -64,3 +68,29 @@ def delete(request, review_pk):
     if request.user == review.user:
         review.delete()
     return redirect('reviews:index')
+
+
+
+@login_required
+def comments_create(request, review_pk):
+    review = Review.objects.get(pk=review_pk)
+    comment_form = CommentForm(request.POST)
+    if comment_form.is_valid():
+        comment = comment_form.save(commit=False)
+        comment.review = review
+        comment.user = request.user
+        comment.save()
+        return redirect('reviews:detail', review.pk)
+    context = {
+        'review': review,
+        'comment_form': comment_form,
+    }
+    return render(request, 'reviews/detail.html', context)
+
+
+@login_required
+def comments_delete(request, review_pk, comment_pk):
+    comment = Comment.objects.get(pk=comment_pk)
+    if request.user == comment.user:
+        comment.delete()
+    return redirect('reviews:detail', review_pk)
