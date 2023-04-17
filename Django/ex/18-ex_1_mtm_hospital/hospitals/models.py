b0bce63a514f16343ae302e0cbc787354fc4cf58
iblug <1115jkb@gmail.com>
2023-04-17 23:09:34 +0900

    Create Django_18-ex_1-hospital

---
+++ Django/ex/18-ex_1_mtm_hospital/hospitals/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Doctor(models.Model):
+    name = models.TextField()
+    # patients = models.ManyToManyField(Patient)
+
+    def __str__(self):
+        return f'{self.pk}번 의사 {self.name}'
+
+
+class Patient(models.Model):
+    # ManyToManyField 작성
+    doctors = models.ManyToManyField(Doctor)
+    name = models.TextField()
+
+    def __str__(self):
+        return f'{self.pk}번 환자 {self.name}'
+
+# Reservation Class 주석 처리
