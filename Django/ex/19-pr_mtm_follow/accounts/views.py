3f2e609ba7acea0057a996b3ac765ffda877351c
iblug <1115jkb@gmail.com>
2023-04-18 23:27:05 +0900

    Create 19-pr_mtm_follow

---
+++ Django/ex/19-pr_mtm_follow/accounts/views.py
@@ -0,0 +1,80 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import get_user_model
+from .forms import CustomUserCreationForm
+
+
+
+def profile(request, username):
+    User = get_user_model()
+    person = User.objects.get(username=username)
+    context = {
+        'person': person,
+    }
+    return render(request, 'accounts/profile.html', context)
+
+
+def login(request):
+    if request.user.is_authenticated:
+        return redirect('articles:index')
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('articles:index')
+    else:
+        form = AuthenticationForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/login.html', context)
+
+
+def logout(request):
+    if request.user.is_authenticated:
+        auth_logout(request)
+    return redirect('articles:index')
+
+
+def signup(request):
+    if request.user.is_authenticated:
+        return redirect('articles:index')
+
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('articles:index')
+    else:
+        form = CustomUserCreationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/signup.html', context)
+
+
+def profile(request, username):
+    User = get_user_model()
+    person = User.objects.get(username=username)
+    context = {
+        'person': person,
+    }
+    return render(request, 'accounts/profile.html', context)
+
+
+def follow(request, user_pk):
+    User = get_user_model()
+    person = User.objects.get(pk=user_pk)
+
+    if request.user != person:
+        if person.followers.filter(pk=request.user.pk).exists():
+            person.followers.remove(request.user)
+        else:
+            person.followers.add(request.user)
+    return redirect('accounts:profile', person.username)
---
+++ Django/ex/19-pr_mtm_follow/readme.md
@@ -0,0 +1,18 @@
+# follow
+* 스켈레톤
+  * 사용자 프로필 조회
+  * 회원가입 / 로그인 / 로그아웃
+  * 게시글 생성, 조회
+  * 댓글 생성, 조회
+* profile
+  * 각 게시글, 댓글 profile 링크
+* follow
+  * follow/unfollow 비대칭 구현
+  * profile 페이지 follow/unfollw 버튼
+    * 비로그인 - 버튼 비활성화
+  * profile 페이지 follow/unfollw 리스트
+    * username 클릭시 해당 프로필 이동
+* pagination
+  * 게시글이 많을 때 페이지 추가
+  * 처음으로, 마지막으로 이동 버튼 추가
+[ ] 팔로우 리스트 모달로 만들기
