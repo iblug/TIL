b232c8f516ad22987b8ec79810f614bf4fc52224
iblug <1115jkb@gmail.com>
2023-04-18 23:22:54 +0900

    Update Django_18-pr-mtm_follow

--- Django/ex/18-ex_2_mtm_article/accounts/views.py
+++ Django/ex/18-ex_2_mtm_article/accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
+from django.contrib.auth import get_user_model
 from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.decorators import login_required
 from .forms import CustomUserCreationForm, CustomUserChangeForm
@@ -82,3 +83,35 @@ def change_password(request):
         'form': form,
     }
     return render(request, 'accounts/change_password.html', context)
+
+
+def profile(request, username):
+    User = get_user_model()
+    person = User.objects.get(username=username)
+    context = {
+        'person': person,
+    }
+    return render(request, 'accounts/profile.html', context)
+
+
+@login_required
+def follow(request, user_pk):
+    # 팔로우를 할 대상이 필요
+    User = get_user_model()
+    person = User.objects.get(pk=user_pk)
+
+    you = User.objects.get(pk=user_pk)  #
+    me = request.user                   #
+    
+    # 나 자신은 팔로우 할 수 없음
+    if you != me:
+        # 팔로우 or 언팔로우
+        if me in you.followers.all():
+        # if person.followers.filter(pk=request.user.pk).exists():
+            # 언팔로우
+            you.followers.remove(me)
+            # me.followings.remove(you)
+        else:
+            you.followers.add(me)
+            # me.following.add(you)
+    return redirect('accounts:profile', you.username)
